e631e61d997bc59ed7108ae77dd36ca549753dd3
Matthew Barham <zonks.zonks@gmail.com>
2022-05-27 10:18:54 -0600

    improve: can print csv records as well as add a new record

--- src/main.rs
+++ src/main.rs
@@ -27,50 +27,173 @@
 
 use std::error::Error;
 use std::{fs, io, process};
-use std::fs::File;
-use std::io::Read;
+use std::fs::{File, OpenOptions};
+use std::io::{Read, Write};
+use std::path::Path;
 use serde::Deserialize;
 
 #[derive(Debug, Deserialize)]
 struct Record {
-    id: i32,
+    id: u16,
     name: String,
     email: String,
 }
 
-fn read_from_csv(contents: String) -> Result<Vec<Record>, Box<dyn Error>> {
+fn read_from_csv(contents: &String) -> (Result<Vec<Record>, Box<dyn Error>>, u16) {
     let mut rdr = csv::Reader::from_reader(contents.as_bytes());
     let mut contents = vec![];
+    let mut count = 0;
     for result in rdr.deserialize::<Record>() {
         match result {
-            Ok(record) => contents.push(record),
-            Err(_) => {}
+            Ok(record) => {
+                contents.push(record);
+                count += 1;
+            },
+            Err(e) => {}
         }
     }
-    Ok(contents)
+    (Ok(contents), count)
 }
 
-fn print_contacts(record: Record) {
+fn write_to_csv(to_write: Record, file: &mut File) -> Result<(), Box<dyn Error>> {
+    let string = to_write.id.to_string() + "," + to_write.name.as_str() + "," + to_write.email.as_str() + "\n";
+
+    file.write_all(string.as_bytes()).expect("failed to write to file");
+
+    Ok(())
+}
+
+fn print_contacts(record: &Record) {
     println!("{:?}", record);
 }
 
-fn main() -> std::io::Result<()> {
-    let mut file = File::open("src/p2_data.csv")?;
-    let mut contents = String::new();
-    file.read_to_string(&mut contents)?;
+fn display_menu() {
+    println!("1) Show contacts");
+    println!("2) Add contact");
+    println!("3) Exit")
+}
 
-    let records = read_from_csv(contents);
-    match records {
-        Ok(records) => {
-            for entry in records
-            {
-                print_contacts(entry);
-            }
-        },
+fn get_csv_input() -> (String, String) {
+    println!("Please enter a name:");
+    let name = get_string_input();
+    println!("Please enter an email:");
+    let email = get_string_input();
+
+    (name.to_owned(), email.to_owned())
+}
+
+fn create_csv_record(id: u16, name: &str, email: &str) -> Record {
+    let entry = Record {
+        id,
+        name: name.to_owned(),
+        email: email.to_owned(),
+    };
+    entry
+}
+
+fn get_string_input() -> String {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_) => {}
         Err(e) => {
-            println!("error running: {}", e);
+            println!("error with input: {}", e);
             process::exit(1);
         }
     }
+
+    let choice = buffer.trim();
+    choice.to_owned()
+}
+
+fn get_integer_input() -> i32 {
+    let mut buffer = String::new();
+    match io::stdin().read_line(&mut buffer) {
+        Ok(_) => {}
+        Err(e) => {
+            println!("error with input: {}", e);
+            process::exit(1);
+        }
+    }
+
+    let choice = buffer.trim().parse::<i32>();
+    match choice {
+        Ok(i) => return i,
+        Err(e) => {
+            println!("error with input: {}", e);
+            process::exit(1);
+        }
+    }
+}
+
+fn main() -> std::io::Result<()> {
+    let mut quit = false;
+    while !quit {
+
+        let file_path = "src/p2_data.csv";
+        let mut file = OpenOptions::new()
+            .read(true)
+            .write(true)
+            .append(true)
+            .open(file_path);
+
+        let mut contents = String::new();
+        let records;
+        match &mut file {
+            Ok(file) => {
+                file.read_to_string(&mut contents)?;
+                records = read_from_csv(&contents);
+                println!("records length: {:?}", &records.1);
+            }
+            Err(e) => {
+                println!("file error: {:?}", e);
+                process::exit(1);
+            }
+        }
+
+        display_menu();
+        match get_integer_input() {
+            1 => {
+                match &records.0 {
+                    Ok(records) => {
+                        for entry in records
+                        {
+                            print_contacts(entry);
+                        }
+                    }
+                    Err(e) => {
+                        println!("error running: {}", e);
+                        process::exit(1);
+                    }
+                }
+            }
+            2 => {
+                let mut file_ref = file.as_mut().unwrap();
+                let strings = get_csv_input();
+                match &records.0 {
+                    Ok(records) => {
+                        let new_id = records.len();
+                        let new_record = create_csv_record(new_id as u16, strings.0.as_str(), strings.1.as_str());
+                        match write_to_csv(new_record, file_ref) {
+                            Ok(_) => {
+                                println!("Successfully wrote to csv");
+                            }
+                            Err(e) => {
+                                println!("error writing to file: {}", e);
+                                process::exit(1);
+                            }
+                        }
+                    }
+                    Err(e) => {
+                        println!("error running: {}", e);
+                        process::exit(1);
+                    }
+                }
+            }
+            _ => {
+                quit = true;
+            }
+        }
+    }
+
     Ok(())
 }
